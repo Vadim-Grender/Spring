ddd66c3ed9ca44274defb413b435d2f5b9f31349
Vadim-Grender <116440694+Vadim-Grender@users.noreply.github.com>
2024-02-26 12:15:39 +0300

    Add files via upload

---
+++ hm7/src/main/java/com/example/hm7/Hm7Application.java
@@ -0,0 +1,13 @@
+package com.example.hm7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hm7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hm7Application.class, args);
+	}
+
+}
